4d6a554c87901a23ff8192f6297b4d551a28764c
Nikshep M P <nikhilmpgowda@gmail.com>
2025-04-23 10:53:21 +0530

    Add project files.

---
+++ Model/PaymentDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Model
+{
+    public class PaymentDetailContext:DbContext
+    {
+        public PaymentDetailContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<PaymentDetail> MyProperty { get; set; }
+
+    }
+}
